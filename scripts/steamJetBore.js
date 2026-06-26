c1f53f1d0c2ba916508242efe12996663b64a8f3
AmbientPush <moscickibartek@gmail.com>
2026-06-26 02:35:03 +0200

    Steam Jet Bore (WIP)

---
+++ scripts/steamJetBore.js
@@ -0,0 +1,5 @@
+const SJB = extend(BeamDrill, "steam-jet-bore", {
+    consumePower(0.5);
+    consumeLiquid(Liquids.hydrogen, 0.5f / 60f);
+    consumeLiquid(Liquids.nitrogen, 3f / 60f).boost();
+})
